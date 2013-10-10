2cc947911d3a2079ba62132ada13ed37f1582cd4
Patrick Lucas <plucas@yelp.com>
2013-10-10 16:25:40 -0700

    Move jarbuilder_test.py into tests/

--- tests/jarbuilder_test.py
+++ tests/jarbuilder_test.py
@@ -1,7 +1,8 @@
 import os
 
 import testify as T
-import jarbuilder as P
+
+from pyleus import jarbuilder
 
 
 class JarbuilderTest(T.TestCase):
@@ -10,12 +11,13 @@ class JarbuilderTest(T.TestCase):
         topology_dir = "foo"
 
         output_arg = "ninja"
-        output_jar = P._build_output_path(output_arg, topology_dir)
+        output_jar = jarbuilder._build_output_path(output_arg, topology_dir)
         T.assert_equals(output_jar, os.path.abspath(output_arg))
 
         output_arg = None
-        output_jar = P._build_output_path(output_arg, topology_dir)
+        output_jar = jarbuilder._build_output_path(output_arg, topology_dir)
         T.assert_equals(output_jar, os.path.abspath(topology_dir) + '.jar')
+
 
 if __name__ == '__main__':
         T.run()
