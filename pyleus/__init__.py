422d4240cee465cc9367866ebc0873114123d128
Patrick Lucas <plucas@yelp.com>
2013-11-05 11:11:22 -0800

    Build and bundle java with dists

--- pyleus/__init__.py
+++ pyleus/__init__.py
@@ -1,4 +1,8 @@
-from __future__ import absolute_import
-
+import os
+import sys
 
 __version__ = '0.0.2'
+
+BASE_JAR = "pyleus-topology-base.jar"
+BASE_JAR_INSTALL_DIR = "share/pyleus"
+BASE_JAR_PATH = os.path.join(sys.prefix, BASE_JAR_INSTALL_DIR, BASE_JAR)
