d5f7c97a057c9ff7647200a43e13a5b9edb1a1a0
Antonio Verardi <antonio@yelp.com>
2013-12-17 15:38:29 -0800

    Changes for manual testing

--- examples/bandwith_monitoring/bandwith_monitoring/traffic_monitor.py
+++ examples/bandwith_monitoring/bandwith_monitoring/traffic_monitor.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division
 
 import logging
-
+import time
 from pyleus.storm import SimpleBolt
 from bandwith_monitoring.traffic_aggregator import Traffic
 
@@ -12,10 +12,11 @@ log = logging.getLogger('traffic_monitor')
 class TrafficMonitorBolt(SimpleBolt):
 
     def initialize(self):
+        log.debug(self.conf)
         self.records = {}
 
     def process_tick(self):
-        log.debug("-------------------------------")
+        log.debug("-----------------------.{0}".format(time.time()))
         for ip_address, traffic in self.records.iteritems():
             log.debug("{0}: {1} B".format(ip_address, traffic))
         self.records.clear()
