5f90b3f8b1af0d1a1b084296c05c48849b3903de
Patrick Lucas <plucas@yelp.com>
2014-02-03 16:17:02 -0800

    Fix help message spacing

--- pyleus/cli/commands/run_subcommand.py
+++ pyleus/cli/commands/run_subcommand.py
@@ -35,11 +35,10 @@ class RunSubCommand(SubCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "topology_dir", metavar="TOPOLOGY_PATH",
-            help="If the path to a directory containing the topology source"
-                 " code is specified, a Pyleus jar will be created on the fly"
-                 " before execution."
-                 " If the path to a Pyleus jar is specified, the jar will be"
-                 "processed for execution immediately.")
+            help="If the path to a directory containing the topology source "
+                 "code is specified, a Pyleus jar will be created on the fly "
+                 "before execution. If the path to a Pyleus jar is specified, "
+                 "the jar will be processed for execution immediately.")
         self.add_specific_arguments(parser)
 
     def run_topology(jar_path, configs):
