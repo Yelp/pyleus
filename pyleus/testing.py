fa2c1080f25083eefd43da6711bcd5aa3a8e5ace
Patrick Lucas <plucas@yelp.com>
2014-05-19 05:48:19 -0700

    Remove deprecated StormComponentTestCase

--- pyleus/testing.py
+++ pyleus/testing.py
@@ -18,16 +18,3 @@ class ComponentTestCase(object):
             input_stream=self.mock_input_stream,
             output_stream=self.mock_output_stream,
         )
-
-
-class StormComponentTestCase(ComponentTestCase):
-    """This class was renamed to ComponentTestCase.
-
-    Remove in next major release.
-    """
-
-    def __init__(self, *args, **kwargs):
-        import warnings
-        warnings.warn("This class has been renamed to ComponentTestCase",
-                      DeprecationWarning)
-        super(StormComponentTestCase, self).__init__(*args, **kwargs)
