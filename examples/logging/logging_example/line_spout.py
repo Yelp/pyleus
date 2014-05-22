cefe00abd913cb6fc087ac110b717c13e04a90f9
Patrick Lucas <plucas@yelp.com>
2014-05-22 07:18:13 -0700

    Add Python logging example topology

---
+++ examples/logging/logging_example/line_spout.py
@@ -0,0 +1,31 @@
+import logging
+import time
+
+from pyleus.storm import Spout
+
+log = logging.getLogger("line_spout")
+
+
+class LineSpout(Spout):
+
+    OUTPUT_FIELDS = ['timestamp']
+
+    INTERVAL = 5 # seconds
+
+    last_emit = None
+
+    def initialize(self):
+        self.last_emit = time.time()
+
+    def next_tuple(self):
+        now = time.time()
+        if now > self.last_emit + self.INTERVAL:
+            self.emit((now,))
+            log.info("Emitted: %r", now)
+            self.last_emit = now
+        else:
+            time.sleep(0.100)
+
+
+if __name__ == '__main__':
+    LineSpout().run()
