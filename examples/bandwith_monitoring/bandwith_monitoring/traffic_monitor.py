47bb18f960c016368be319049c294a7fb8c65007
Antonio Verardi <antonio@yelp.com>
2014-06-04 09:20:09 -0700

    Minor changes

--- examples/bandwith_monitoring/bandwith_monitoring/traffic_monitor.py
+++ examples/bandwith_monitoring/bandwith_monitoring/traffic_monitor.py
@@ -12,11 +12,10 @@ log = logging.getLogger('traffic_monitor')
 class TrafficMonitorBolt(SimpleBolt):
 
     def initialize(self):
-        log.debug(self.conf)
         self.records = {}
 
     def process_tick(self):
-        log.debug("-----------------------.{0}".format(time.time()))
+        log.debug("-----------------------{0}".format(time.time()))
         for ip_address, traffic in self.records.iteritems():
             log.debug("{0}: {1} B".format(ip_address, traffic))
         self.records.clear()
