b690ac880b4548c757e9b0e11dd16f07ea72e24d
Antonio Verardi <antonio@yelp.com>
2014-10-06 10:03:35 -0700

    Excpetions docstrings and pruning
    TopologyError and DependenciesError were deleted since not used.

--- pyleus/exception.py
+++ pyleus/exception.py
@@ -4,24 +4,44 @@ from __future__ import absolute_import
 
 
 class PyleusError(Exception):
-    """Base class for pyleus specific exceptions"""
+    """Base class for pyleus specific exceptions."""
     def __str__(self):
         return "[{0}] {1}".format(type(self).__name__,
                ", ".join(str(i) for i in self.args))
 
 
-class ConfigurationError(PyleusError): pass
+class ConfigurationError(PyleusError):
+    """Raised when a required configuration value missing or misformed. You can
+    usually specify these values as command line options or in pyleus config
+    file.
+    """
+    pass
 
-class JarError(PyleusError): pass
 
-class TopologyError(PyleusError): pass
-class InvalidTopologyError(TopologyError): pass
-class DependenciesError(TopologyError): pass
+class JarError(PyleusError):
+    """Raised when a problem occurred with pyleus base jar."""
+    pass
 
-class VirtualenvError(PyleusError): pass
-class StormError(PyleusError): pass
+
+class InvalidTopologyError(PyleusError):
+    """Raised when topology validation failed. Usually it is due to a syntax
+    error or a mispelled name in the topology definition YAML file.
+    """
+    pass
+
+
+class VirtualenvError(PyleusError):
+    """Raised when an error occurred during virtualenv creation, dependencies
+    installation or modules invocation.
+    """
+    pass
+
+
+class StormError(PyleusError):
+    """Raised when Storm invocation terminates with errors."""
+    pass
 
 
 def command_error_fmt(cmd_name, exception):
-    """Error message for commands based on exception"""
+    """Formats error message given command and exception."""
     return "pyleus {0}: error: {1}".format(cmd_name, str(exception))
