c757d4ecc09374c053481b20114509c231fb1898
Patrick Lucas <plucas@yelp.com>
2015-06-05 12:23:12 -0700

    v0.3.0 release

--- pyleus/__init__.py
+++ pyleus/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import pkg_resources
 
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 
 BASE_JAR = "pyleus-base.jar"
 BASE_JAR_PATH = pkg_resources.resource_filename('pyleus', BASE_JAR)
